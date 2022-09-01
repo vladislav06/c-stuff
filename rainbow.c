bce21248d5120ce7b65d859ece9786ca8244123c
VM <mrkevinpro61@gmail.com>
2022-09-01 13:13:49 +0300

    changed rainbow generator

--- rainbow.c
+++ rainbow.c
@@ -1,27 +1,26 @@
 #include <math.h>
 #include <stdio.h>
 
+void print_rainbow(float shift, float speed) {
   int cnt = 0;
-void print_rainbow() {
+  while (cnt < 100) {
+    int r, g, b;
 
-  float freq = 0.1f;
-  int r, g, b;
+    r = std::sin(zoom * cnt + 0 + shift) * 255;
+    g = std::sin(zoom * cnt + 2 + shift) * 255;
+    b = std::sin(zoom * cnt + 4 + shift) * 255;
+    cnt++;
 
-  r = std::sin(freq * cnt + 0) * 255;
-  g = std::sin(freq * cnt + 2) * 255;
-  b = std::sin(freq * cnt + 4) * 255;
-  cnt++;
- printf("r:%4d, g:%4d, b:%4d",r,g,b);
-  printf("\033[48;2;%d;%d;%d m  \033[0m\n", (int)(r ), (int)(g ),
-         (int)(b ));
-
+    // printf("r:%4d, g:%4d, b:%4d",r,g,b);
+    printf("\033[48;2;%d;%d;%d m  \033[0m", (int)(r), (int)(g), (int)(b));
+  }
 }
 int main() {
 
-while(true){
-  print_rainbow();
-}
+  print_rainbow(1, 0.1);
+  printf("\n");
 
+  print_rainbow(2, 0.1);
 
   return 0;
 }
